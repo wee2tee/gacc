3d0ed46fcadfbab22eb435978e5c3f9e5a4c176a
WEE <WEE@WEE-DELL>
2014-10-20 14:50:54 +0700

    before using Multiple Database

--- protected/views/appform/gljnl_p.php
+++ protected/views/appform/gljnl_p.php
@@ -1,5 +1,8 @@
 <script>
-    $(function(){ $('#just-click').on('click',function(){ alert('This is a great job.'); }); });
+    //$(function(){ $('#just-click').on('click',function(){ alert('This is a great job.'); }); });
+    function greatJob<?php echo $appform_id ?>(){
+        alert("This is a great job.");
+    }
 </script>
 
-<div class="appform-root" id="<?php echo $appform_id ?>"> This is an Application Form GL(Posted)<input type='button' value='just click' id='just-click'/></div>
+<div class="appform-root" id="<?php echo $appform_id ?>"> This is an Application Form GL(Posted)<input type='button' value='just click' onclick="return greatJob<?php echo $appform_id ?>()" /></div>
